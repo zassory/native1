17373390a3e28519490b84aded27d1ea3f34a652
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-12-04 15:06:05 -0300

    objetos literales

--- src/typescript/ObjetosLiterales.tsx
+++ src/typescript/ObjetosLiterales.tsx
@@ -1,8 +1,37 @@
 
+interface Persona {
+    nombreCompleto: string;
+    edad: number;
+    direccion: Direccion;
+}
+
+interface Direccion {
+    pais: string;
+    casaNo: number;
+}
+
+
 export const ObjetosLiterales = () => {
+
+  const persona: Persona = {
+    nombreCompleto: 'Nicolas',
+    edad: 40,
+    direccion: {
+        pais:'Argentina',
+        casaNo: 3036
+    }
+  }
+  
+  
+
   return (
     <>
-      <h3>Objetos Literales</h3>  
+      <h3>Objetos Literales</h3>
+      <code>
+        <pre>
+            {JSON.stringify(persona, null , 2)}
+        </pre>
+      </code>
     </>
   )
 }
